5a15cf339e3acd9d63aff942cc19a7349063961b
zhrimur <zhrimur42@gmail.com>
2021-11-23 19:05:11 +0300

    #7

---
+++ #7/script.js
@@ -0,0 +1,26 @@
+//"Привет".getVowels(). Замыкание можно убрать и оставить просто подсчет гласных
+String.prototype.getVowels =
+  function(vowel) {
+    let stringVowels = "уеёыаоэяиюУЕЁЫАОЭЯИЮ";
+    let vowelsCounter = 0;
+    if (vowel === undefined) {
+      for (let i = 0; i < this.length; i++) {
+        if (stringVowels.search(this[i]) > -1) {
+          vowelsCounter++;
+        }
+      }
+    } else {
+      if (stringVowels.search(vowel) > -1) {
+        vowel = vowel.toUpperCase() + vowel.toLowerCase();
+        for (let i = 0; i < this.length; i++) {
+          if (vowel.search(this[i]) > -1) {
+            vowelsCounter++;
+          }
+        }
+      } else {
+        console.log("Укажите гласную или оставьте скобки пустыми");
+      }
+    }
+    return vowelsCounter;
+  }
+
