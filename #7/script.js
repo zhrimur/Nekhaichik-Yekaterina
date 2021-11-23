502fd3ed0f10e36213afcfd4f7fd0d9487de57c4
zhrimur <zhrimur42@gmail.com>
2021-11-23 19:37:01 +0300

    #7

--- #7/script.js
+++ #7/script.js
@@ -1,26 +1,23 @@
-//"Привет".getVowels(). Замыкание можно убрать и оставить просто подсчет гласных
-String.prototype.getVowels =
-  function(vowel) {
-    let stringVowels = "уеёыаоэяиюУЕЁЫАОЭЯИЮ";
-    let vowelsCounter = 0;
-    if (vowel === undefined) {
+String.prototype.getVowels = function (vowel) {
+  let stringVowels = "уеёыаоэяиюУЕЁЫАОЭЯИЮ";
+  let vowelsCounter = 0;
+  if (vowel === undefined) {
+    for (let i = 0; i < this.length; i++) {
+      if (stringVowels.search(this[i]) > -1) {
+        vowelsCounter++;
+      }
+    }
+  } else {
+    if (stringVowels.search(vowel) > -1) {
+      vowel = vowel.toUpperCase() + vowel.toLowerCase();
       for (let i = 0; i < this.length; i++) {
-        if (stringVowels.search(this[i]) > -1) {
+        if (vowel.search(this[i]) > -1) {
           vowelsCounter++;
         }
       }
     } else {
-      if (stringVowels.search(vowel) > -1) {
-        vowel = vowel.toUpperCase() + vowel.toLowerCase();
-        for (let i = 0; i < this.length; i++) {
-          if (vowel.search(this[i]) > -1) {
-            vowelsCounter++;
-          }
-        }
-      } else {
-        console.log("Укажите гласную или оставьте скобки пустыми");
-      }
+      console.log("Укажите гласную или оставьте скобки пустыми");
     }
-    return vowelsCounter;
   }
-
+  return vowelsCounter;
+};
